e956e8ae624c69fa2a4338b2ab99b55f56b28ac1
fanfanhome <364549495@qq.com>
2020-11-29 18:33:40 +0800

    first commit

---
+++ FMFramework.framework/Headers/FFTest.h
@@ -0,0 +1,17 @@
+//
+//  FFTest.h
+//  FMFramework
+//
+//  Created by 范孟磊 on 2020/11/29.
+//  Copyright © 2020 范. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FFTest : NSObject
++(void)Log;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FMFramework.framework/Headers/FMFramework.h
@@ -0,0 +1,19 @@
+//
+//  FMFramework.h
+//  FMFramework
+//
+//  Created by 范孟磊 on 2020/11/29.
+//  Copyright © 2020 范. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FMFramework.
+FOUNDATION_EXPORT double FMFrameworkVersionNumber;
+
+//! Project version string for FMFramework.
+FOUNDATION_EXPORT const unsigned char FMFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FMFramework/PublicHeader.h>
+
+#import "FFTest.h"
